87994353a7a1980915c66daedc2311e08a307193
Justin Collier <twoquestions@gmail.com>
2024-09-27 20:49:38 -0400

    getting authjs setup

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
+export default {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    // 3. Append the path to the Skeleton package
+    join(require.resolve(
+      '@skeletonlabs/skeleton'),
+      '../**/*.{html,js,svelte,ts}'
+    )
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, forms, skeleton({
+    themes: { preset: ["skeleton"] }
+  })],
+} satisfies Config;
+
